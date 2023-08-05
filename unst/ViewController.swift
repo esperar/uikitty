1dc667c7afdcffe2b2a594533ef5144c5aac6d6d
aesperer <piyrw9754@gmail.com>
2023-08-06 00:55:22 +0900

    back button

--- unst/ViewController.swift
+++ unst/ViewController.swift
@@ -1,19 +1,42 @@
-//
-//  ViewController.swift
-//  unst
-//
-//  Created by 김희망 on 2023/08/06.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = ""
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    lazy var backButton: UIButton = {
+        let button = UIButton()
+        
+        button.setTitle("Back Button", for: .normal)
+        button.setTitleColor(.systemBlue, for: .normal)
+        button.addTarget(self, action: #selector(tapBackButton), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        self.view.backgroundColor = .white
+        
+        self.view.addSubview(titleLabel)
+        self.view.addSubview(backButton)
+        
+        NSLayoutConstraint.activate([
+            backButton.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 100),
+            backButton.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 50),
+            
+            titleLabel.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor, constant: -50),
+            titleLabel.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor, constant: -100)
+        ])
     }
-
-
+    
+    @objc func tapBackButton() {
+        // TODO: Implement back button action
+    }
 }
-
